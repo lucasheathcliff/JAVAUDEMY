d272ff547c854e39e13a30f474ef5aa67464b459
lucasheathcliff <lucassamuel130@hotmail.com>
2023-08-24 11:53:49 -0300

    Correção de pastas.2

---
+++ Sessao8/Produtos/src/produto/Produto.java
@@ -0,0 +1,18 @@
+package produto;
+
+public class Produto {
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public double vlrTotalEstoque(){
+        return quantidade * preco;
+    }
+    public void addProduto(int quant) {
+        quantidade = quantidade + quant;
+    }
+    public void removerProduto(int quanty){
+        quantidade = quantidade - quanty;
+    }
+
+}
